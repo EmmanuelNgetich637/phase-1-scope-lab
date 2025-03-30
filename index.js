ba9314ccc20080cb2621ae6346859fbfafc24042
Emmanuel Ngetich <engetich333@gmail.com>
2025-03-30 00:20:14 -0700

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// Global variable declaration
+var customerName = 'bob';
+
+// 1. Access the global customerName
+function returnsCustomerName() {
+  return customerName;
+}
+
+// 2. Modify the global customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Set a global bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicit global declaration (no var/let/const)
+}
+
+// 4. Attempt to change a constant
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // Will work if bestCustomer was not declared with const
+}
+
+// 5. Attempt to change a constant (should fail)
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error when called (which is what the test expects)
+  leastFavoriteCustomer = 'someone else';
+}
